fed10d143d34df147cee8a25299391ea9e848a3e
SATO Sho <shogidemo@gmail.com>
2023-04-25 18:36:41 +0900

    delete機能まで実装できました！

--- src/main/java/com/example/todo/app/todo/TodoController.java
+++ src/main/java/com/example/todo/app/todo/TodoController.java
@@ -3,11 +3,13 @@ package com.example.todo.app.todo;
 import java.util.Collection;
 
 import javax.inject.Inject;
-import javax.validation.Valid;
+import javax.validation.groups.Default;
+//import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +19,9 @@ import org.terasoluna.gfw.common.exception.BusinessException;
 import org.terasoluna.gfw.common.message.ResultMessage;
 import org.terasoluna.gfw.common.message.ResultMessages;
 
+import com.example.todo.app.todo.TodoForm.TodoCreate;
+import com.example.todo.app.todo.TodoForm.TodoDelete;
+import com.example.todo.app.todo.TodoForm.TodoFinish;
 import com.example.todo.domain.model.Todo;
 import com.example.todo.domain.service.todo.TodoService;
 import com.github.dozermapper.core.Mapper;
@@ -44,7 +49,11 @@ public class TodoController {
 	}
 	
 	@PostMapping("create")
-	public String create(@Valid TodoForm todoForm, BindingResult bindingResult, Model model, RedirectAttributes attributes) {
+	public String create(@Validated({ Default.class, TodoCreate.class }) 
+						 TodoForm todoForm, 
+			 			 BindingResult bindingResult, 
+			 			 Model model, 
+			 			 RedirectAttributes attributes) {
 		if (bindingResult.hasErrors()) {
 			return list(model);
 		}
@@ -64,4 +73,48 @@ public class TodoController {
 		//PRG(Post-Redirect-Get)パターンで、list画面にリダイレクトし、クライアント側からGetメソッドでlistフォームを取得することで、POSTメソッドの再実行を防ぐことができる。
 		return "redirect:/todo/list";
 	}
+	
+    @PostMapping("finish")
+    public String finish(
+            @Validated({ Default.class, TodoFinish.class }) TodoForm form,
+            BindingResult bindingResult, Model model,
+            RedirectAttributes attributes) {
+    	
+        if (bindingResult.hasErrors()) {
+            return list(model);
+        }
+
+        try {
+            todoService.finish(form.getTodoId());
+        } catch (BusinessException e) {
+            model.addAttribute(e.getResultMessages());
+            return list(model);
+        }
+
+        attributes.addFlashAttribute(ResultMessages.success().add(
+                ResultMessage.fromText("Finished successfully!")));
+        return "redirect:/todo/list";
+    }
+    
+    @PostMapping("delete")
+    public String delete(
+            @Validated({ Default.class, TodoDelete.class }) TodoForm form,
+            BindingResult bindingResult, Model model,
+            RedirectAttributes attributes) {
+
+        if (bindingResult.hasErrors()) {
+            return list(model);
+        }
+
+        try {
+            todoService.delete(form.getTodoId());
+        } catch (BusinessException e) {
+            model.addAttribute(e.getResultMessages());
+            return list(model);
+        }
+
+        attributes.addFlashAttribute(ResultMessages.success().add(
+                ResultMessage.fromText("Deleted successfully!")));
+        return "redirect:/todo/list";
+    }
 }
--- src/main/webapp/WEB-INF/views/todo/list.jsp
+++ src/main/webapp/WEB-INF/views/todo/list.jsp
@@ -7,6 +7,30 @@
 .strike {
     text-decoration: line-through;
 }
+
+.inline {
+    display: inline-block;
+}
+
+.alert {
+    border: 1px solid;
+}
+
+.alert-error {
+    background-color: #c60f13;
+    border-color: #970b0e;
+    color: white;
+}
+
+.alert-success {
+    background-color: #5da423;
+    border-color: #457a1a;
+    color: white;
+}
+
+.text-error {
+    color: #c60f13;
+}
 </style>
 </head>
 <body>
@@ -18,7 +42,7 @@
     		action="${pageContext.request.contextPath}/todo/create"
     		method="post" modelAttribute="todoForm">
     		<form:input  path="todoTitle" />
-    		<form:errors path="todoTitle" />
+    		<form:errors path="todoTitle" cssClass="text-error" />
     		<form:button>Create Todo</form:button>    		    		
     	</form:form>
     </div>
@@ -34,8 +58,26 @@
                         </c:when>
                         <c:otherwise>
                             ${f:h(todo.todoTitle)}
+                            <form:form
+                                action="${pageContext.request.contextPath}/todo/finish"
+                                method="post"
+                                modelAttribute="todoForm"
+                                cssClass="inline">
+                                <form:hidden path="todoId"
+                                    value="${f:h(todo.todoId)}" />
+                                <form:button>Finish</form:button>
+                            </form:form>
                          </c:otherwise>
-                    </c:choose></li>
+                    </c:choose>
+                        <form:form
+	                        action="${pageContext.request.contextPath}/todo/delete"
+	                        method="post" modelAttribute="todoForm"
+	                        cssClass="inline">
+	                        <form:hidden path="todoId"
+	                            value="${f:h(todo.todoId)}" />
+	                        <form:button>Delete</form:button>
+                    </form:form>
+                    </li>
             </c:forEach>
         </ul>
     </div>
