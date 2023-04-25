ce6185fa0044fbd66e01136c30aa5cc4ba04ea85
SATO Sho <shogidemo@gmail.com>
2023-04-25 14:11:44 +0900

    途中

--- src/main/java/com/example/todo/app/todo/TodoController.java
+++ src/main/java/com/example/todo/app/todo/TodoController.java
@@ -3,21 +3,32 @@ package com.example.todo.app.todo;
 import java.util.Collection;
 
 import javax.inject.Inject;
+import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.terasoluna.gfw.common.exception.BusinessException;
+import org.terasoluna.gfw.common.message.ResultMessage;
+import org.terasoluna.gfw.common.message.ResultMessages;
 
 import com.example.todo.domain.model.Todo;
 import com.example.todo.domain.service.todo.TodoService;
+import com.github.dozermapper.core.Mapper;
 
 @Controller
 @RequestMapping("todo")
 public class TodoController {
 	@Inject
 	TodoService todoService;
+	
+	@Inject
+	Mapper beanMapper;
 	
 	@ModelAttribute
 	public TodoForm setUpForm() {
@@ -30,5 +41,27 @@ public class TodoController {
 		Collection<Todo> todos = todoService.findAll();
 		model.addAttribute("todos",todos);
 		return "todo/list";
+	}
+	
+	@PostMapping("create")
+	public String create(@Valid TodoForm todoForm, BindingResult bindingResult, Model model, RedirectAttributes attributes) {
+		if (bindingResult.hasErrors()) {
+			return list(model);
+		}
+		
+		Todo todo = beanMapper.map(todoForm, Todo.class);
+		
+		try {
+			todoService.create(todo);
+		} catch (BusinessException e) {
+			model.addAttribute(e.getResultMessages());
+			return list(model);
+		}
+		
+		
+		attributes.addFlashAttribute(ResultMessages.success().add(ResultMessage.fromText("Created successfully!")));
+		
+		//PRG(Post-Redirect-Get)パターンで、list画面にリダイレクトし、クライアント側からGetメソッドでlistフォームを取得することで、POSTメソッドの再実行を防ぐことができる。
+		return "redirect:/todo/list";
 	}
 }
